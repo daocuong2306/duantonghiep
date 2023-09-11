714d6d168f5967546d0129efc90a7beee5a3eaf9
DESKTOP-N479MS7\Gia Khanhs <ggia91188@gmail.com>
2023-09-11 23:37:01 +0700

    ghép template

--- backend_laravel_farmwork/resources/views/category/add.blade.php
+++ backend_laravel_farmwork/resources/views/category/add.blade.php
@@ -1,14 +1,17 @@
+@extends('template.category')
+@section('content')
 <form action="{{route('category_add')}}" method="POST" enctype="multipart/form-data">
     @csrf
-    <h1 class="ml-5 mb-3">Category </h1>
-    <div class="mb-5 px-5">
+    <div class="">
       <label class="form-label"><h5>Name</h5></label>
       <input type="text" name="name" class="form-control">
     </div>
-    <div class="mb-5 px-5">
+    <div class="">
         <label class="form-label"><h5>Image</h5></label>
         <input type="file" name="image">
       </div>
     
-    <button class="mt-3" type="submit" class="btn btn-primary ml-5 ">Thêm</button>
+      <button  class="btn btn-primary"><a style="text-decoration: none; color: white;" href="">Add</a></button>
+      <button  class="btn btn-primary"><a style="text-decoration: none; color: white;" href="/category">Category List</a></button>
 </form>
+@endsection
--- backend_laravel_farmwork/routes/web.php
+++ backend_laravel_farmwork/routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('template/admin');
 });
 
 //Category nè 
-Route::get('/index',[App\Http\Controllers\CategoryController::class,'index'])->name('category_index');
+Route::get('/category',[App\Http\Controllers\CategoryController::class,'index'])->name('category_index');
 Route::match(['GET','POST'],'/add',[App\Http\Controllers\CategoryController::class,'add'])->name('category_add');
 Route::match(['GET','POST'],'/edit/{id}',[App\Http\Controllers\CategoryController::class,'edit'])->name('category_edit');
 Route::get('/delete/{id}',[App\Http\Controllers\CategoryController::class,'delete'])->name('category_delete');
