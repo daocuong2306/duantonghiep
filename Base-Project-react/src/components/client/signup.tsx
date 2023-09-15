1f3e8790795fc6f20b22cc3616705856ef2f24bd
daocuong2306 <115203956+daocuong2306@users.noreply.github.com>
2023-09-15 21:57:34 +0700

    fix login

--- Base-Project-react/src/components/client/signup.tsx
+++ Base-Project-react/src/components/client/signup.tsx
@@ -3,7 +3,7 @@ import { IUser } from "@/interface/user"
 import { useAppDispatch } from "@/store/hook"
 import { useForm } from "react-hook-form"
 import { Link, useNavigate } from "react-router-dom"
-type signUpForm={
+type signUpForm = {
     id?: number;
     name: null;
     email: string;
@@ -12,31 +12,31 @@ type signUpForm={
     phone: null;
     address: null;
     password: string;
-    confirmPassword:string
+    confirmPassword: string
 }
 const Signup = () => {
-    const [addUser]=useAddUserMutation()
-    const dispatch=useAppDispatch()
-    const url=useNavigate()
-    const {data}=useGetUserQuery()
-    const {register,handleSubmit}=useForm()
-    const onHandleSubmit=(dataUser:signUpForm)=>{
-        if(dataUser.password!==dataUser.confirmPassword){
+    const [addUser] = useAddUserMutation()
+    const dispatch = useAppDispatch()
+    const url = useNavigate()
+    const { data } = useGetUserQuery()
+    const { register, handleSubmit } = useForm()
+    const onHandleSubmit = (dataUser: signUpForm) => {
+        if (dataUser.password !== dataUser.confirmPassword) {
             console.log("lỗi confirm");
-        }else{
-            for(let user of data){
-                if(user.email===dataUser.email){
+        } else {
+            for (let user of data) {
+                if (user.email === dataUser.email) {
                     console.log("email đã tồn tại");
-                }else{
-                    const {confirmPassword,...newUser}=dataUser
-                    const newUserData={...newUser,role:1}
+                } else {
+                    const { confirmPassword, ...newUser } = dataUser
+                    const newUserData = { ...newUser, role: 1 }
                     addUser(newUserData)
                     console.log(newUserData);
-                    
+                    url('/login')
                 }
             }
         }
-        
+
     }
     return (
         <div>
@@ -56,19 +56,19 @@ const Signup = () => {
                                 <div>
                                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                                     <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required
-                                    {...register("email")}
+                                        {...register("email")}
                                     />
                                 </div>
                                 <div>
                                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                    <input type="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required 
-                                    {...register("password")}
+                                    <input type="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required
+                                        {...register("password")}
                                     />
                                 </div>
                                 <div>
                                     <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                                    <input type="password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required 
-                                    {...register("confirmPassword")}
+                                    <input type="password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required
+                                        {...register("confirmPassword")}
                                     />
                                 </div>
                                 <div className="flex items-start">
