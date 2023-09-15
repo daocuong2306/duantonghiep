7e850ec5f0226a5f5b3b256327b3ff0c1661b7d3
datnt210803 <137571347+datnt210803@users.noreply.github.com>
2023-09-15 21:24:53 +0700

    signup

--- Base-Project-react/src/components/client/signup.tsx
+++ Base-Project-react/src/components/client/signup.tsx
@@ -1,6 +1,43 @@
-import { Link } from "react-router-dom"
-
+import { useAddUserMutation, useGetUserQuery } from "@/api/user"
+import { IUser } from "@/interface/user"
+import { useAppDispatch } from "@/store/hook"
+import { useForm } from "react-hook-form"
+import { Link, useNavigate } from "react-router-dom"
+type signUpForm={
+    id?: number;
+    name: null;
+    email: string;
+    image: null;
+    role: number;
+    phone: null;
+    address: null;
+    password: string;
+    confirmPassword:string
+}
 const Signup = () => {
+    const [addUser]=useAddUserMutation()
+    const dispatch=useAppDispatch()
+    const url=useNavigate()
+    const {data}=useGetUserQuery()
+    const {register,handleSubmit}=useForm()
+    const onHandleSubmit=(dataUser:signUpForm)=>{
+        if(dataUser.password!==dataUser.confirmPassword){
+            console.log("lỗi confirm");
+        }else{
+            for(let user of data){
+                if(user.email===dataUser.email){
+                    console.log("email đã tồn tại");
+                }else{
+                    const {confirmPassword,...newUser}=dataUser
+                    const newUserData={...newUser,role:1}
+                    addUser(newUserData)
+                    console.log(newUserData);
+                    
+                }
+            }
+        }
+        
+    }
     return (
         <div>
             <section className="bg-gray-50 dark:bg-gray-900">
@@ -14,18 +51,25 @@ const Signup = () => {
                             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                                 Create and account
                             </h1>
-                            <form className="space-y-4 md:space-y-6" action="#">
+                            <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit(onHandleSubmit)}>
+
                                 <div>
                                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                                    <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required
+                                    {...register("email")}
+                                    />
                                 </div>
                                 <div>
                                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                    <input type="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required 
+                                    {...register("password")}
+                                    />
                                 </div>
                                 <div>
                                     <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                                    <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                    <input type="password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required 
+                                    {...register("confirmPassword")}
+                                    />
                                 </div>
                                 <div className="flex items-start">
                                     <div className="flex items-center h-5">
@@ -35,7 +79,7 @@ const Signup = () => {
                                         <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
                                     </div>
                                 </div>
-                                <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+                                <button type="submit" className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
                                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                     Already have an account?   <Link to={'/login'} className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
                                 </p>
