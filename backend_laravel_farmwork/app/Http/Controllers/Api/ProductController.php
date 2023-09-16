57ad5316aab9de0355b365174671cabf51687475
Trongnguyen29603 <viettrong29603@gmail.com>
2023-09-16 23:16:43 +0700

    apiproduct

---
+++ backend_laravel_farmwork/app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Validator as ValidationValidator;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::all();
+        if($products->count()>0){
+            return response()->json([
+                'status'=>200,
+                'product'=>$products,
+            ],200);
+        }else{
+            return response()->json([
+                'status'=>200,
+                'message'=>'not found'
+            ],400);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'name'=>'required|string|max:191',
+            'prices'=>'required',
+            'description'=>'required|string|max:191',
+            'status'=>'required|string|max:191',
+            'image'=>'required|string|max:191',
+            'code'=>'required|string|max:191',
+            'id_category'=>'required'
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'status'=>422,
+                'errors'=>$validator->messages(),
+            ],422);
+        }else{
+            $product=Product::create([
+                'name'=>$request->name,
+                'prices'=>$request->prices,
+                'description'=>$request->description,
+                'status'=>$request->status,
+                'image'=>$request->image,
+                'code'=>$request->code,
+                'id_category'=>$request->id_category
+            ]);
+
+            if($product){
+                return response()->json([
+                    'status'=>200,
+                    'message'=>'Successfull',
+                ],200);
+            }else{
+                return response()->json([
+                    'status'=>500,
+                    'message'=>'Wrong',
+                ],500);
+            }
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+        if($product){
+            return response()->json([
+                'status'=>200,
+                'product'=>$product,
+            ],200);
+        }else{
+            return response()->json([
+                'status'=>404,
+                'message'=>'Not found',
+            ],404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(),[
+            'name'=>'required|string|max:191',
+            'prices'=>'required',
+            'description'=>'required|string|max:191',
+            'status'=>'required|string|max:191',
+            'image'=>'required|string|max:191',
+            'code'=>'required|string|max:191',
+            'id_category'=>'required'
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'status'=>422,
+                'errors'=>$validator->messages(),
+            ],422);
+        }else{
+            $product = Product::find($id);
+            $product=Product::create([
+                'name'=>$request->name,
+                'prices'=>$request->prices,
+                'description'=>$request->description,
+                'status'=>$request->status,
+                'image'=>$request->image,
+                'code'=>$request->code,
+                'id_category'=>$request->id_category
+            ]);
+
+            if($product){
+                return response()->json([
+                    'status'=>200,
+                    'message'=>'Successfull',
+                ],200);
+            }else{
+                return response()->json([
+                    'status'=>500,
+                    'message'=>'Wrong',
+                ],500);
+            }
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        if($product){
+            $product ->delete();
+            return response()->json([
+                'status'=>200,
+                'message'=>'Delete Successfull',
+            ],200);
+        }else{
+            return response()->json([
+                'status'=>404,
+                'message'=>'Not found',
+            ],404);
+        }
+    }
+}
