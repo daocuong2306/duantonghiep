4d2af64f270cd7e1be4e25c379965fdd7dcfb8bd
DESKTOP-N479MS7\Gia Khanhs <ggia91188@gmail.com>
2023-09-17 22:04:11 +0700

    product thêm s

--- backend_laravel_farmwork/routes/api.php
+++ backend_laravel_farmwork/routes/api.php
@@ -26,8 +26,8 @@ Route::get('category/edit/{id}',[CategoryController::class,'edit']);
 Route::put('category/edit/{id}',[CategoryController::class,'update']);
 Route::delete('category/delete/{id}',[CategoryController::class,'destroy']);
 
-Route::get('product',[ProductController::class,'index']);
-Route::post('product',[ProductController::class,'store']);
-Route::get('product/{id}',[ProductController::class,'show']);
-Route::put('product/edit/{id}',[ProductController::class,'update']);
-Route::delete('product/delete/{id}',[ProductController::class,'destroy']);
+Route::get('products',[ProductController::class,'index']);
+Route::post('products',[ProductController::class,'store']);
+Route::get('products/{id}',[ProductController::class,'show']);
+Route::put('products/edit/{id}',[ProductController::class,'update']);
+Route::delete('products/delete/{id}',[ProductController::class,'destroy']);
