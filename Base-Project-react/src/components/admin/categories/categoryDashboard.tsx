5231ff5a486060c6a46b4dadf6f6e5bf783dfaef
datnt210803 <137571347+datnt210803@users.noreply.github.com>
2023-09-20 17:01:42 +0700

    updateCategory and fix another file

--- Base-Project-react/src/components/admin/categories/categoryDashboard.tsx
+++ Base-Project-react/src/components/admin/categories/categoryDashboard.tsx
@@ -1,10 +1,16 @@
+import { useGetCategoriesQuery, useRemoveCategoryMutation } from "@/api/category"
+import { ICategory } from "@/interface/category"
+import { FcFullTrash, FcSupport } from "react-icons/fc"
 import { Link } from "react-router-dom"
 
 const CategoryDashboard = () => {
+    const { data: categories, error, isLoading } = useGetCategoriesQuery()
+    const [deleteCate]=useRemoveCategoryMutation()
+    const deleteCategory=(id:Number)=>{
+        deleteCate(Number(id))
+    }
     return (
         <div>
-
-
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
                 <div className="pb-4 bg-white dark:bg-white-900">
                     <label htmlFor="table-search" className="sr-only">Search</label>
@@ -20,7 +26,7 @@ const CategoryDashboard = () => {
                 <div>
                     <Link to="/admin/categories/add"><button className="bg-blue-400 p-[10px] rounded-lg text-bold text-white">Add new category</button></Link>
                 </div>
-                <table className="w-full text-sm text-left text-white-500 dark:text-white-400">
+                <table className="w-full text-sm text-center text-white-500 dark:text-white-400">
                     <thead className="text-xs text-white-700 uppercase bg-white-50 dark:bg-white-700 dark:text-white-400">
                         <tr>
                             <th scope="col" className="p-4">
@@ -32,22 +38,42 @@ const CategoryDashboard = () => {
                             <th scope="col" className="px-6 py-3">
                                 Category name
                             </th>
+                            <th scope="col" className="px-6 py-3">
+                                Image
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Action
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className="bg-white border-b dark:bg-white-800 dark:border-white-700  ">
-                            <td className="w-4 p-4">
-                                <div className="flex items-center">
-                                    <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-white-100 border-white-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-white-800 dark:focus:ring-offset-white-800 focus:ring-2 dark:bg-white-700 dark:border-white-600" />
-                                    <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
-                                </div>
-                            </td>
-                            <th scope="row" className="px-6 py-4 font-medium text-black-900 whitespace-nowrap dark:text-black hover:text-blue-500">
-                                Apple MacBook Pro 17"
-                            </th>
-                            
-                        </tr>
+                        {categories?.categories.map((category: ICategory) =>
+                            <tr className="bg-white border-b dark:bg-white-800 dark:border-white-700  ">
+                                <td className="w-4 p-4">
+                                    <div className="flex items-center">
+                                        <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-white-100 border-white-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-white-800 dark:focus:ring-offset-white-800 focus:ring-2 dark:bg-white-700 dark:border-white-600" />
+                                        <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
+                                    </div>
+                                </td>
+                                <th scope="row" className="px-6 py-4 font-medium text-black-900 whitespace-nowrap dark:text-black hover:text-blue-500">
+                                    {category?.name}
+                                </th>
+                                <th scope="row" className="px-6 py-4 font-medium text-black-900 whitespace-nowrap dark:text-black hover:text-blue-500">
+                                    <img src={category?.image} className="m-auto w-10 h-10"/>
+                                </th>
+                                <th scope="row" className="px-6 py-4 font-medium text-black-900 whitespace-nowrap dark:text-black hover:text-blue-500">
+                                    <div className="flex ">
+                                        <Link to={`/admin/categories/update/${category?.id}`}>
+                                            <FcSupport className='w-6 h-6 blue mr-2'/>
+                                        </Link>
+                                        <Link to="">
+                                            <FcFullTrash className='w-6 h-6' onClick={()=>deleteCategory(Number(category?.id))} />
+                                        </Link>
+                                    </div>
+                                </th>
 
+                            </tr>
+                        )}
                     </tbody>
                 </table>
             </div>
