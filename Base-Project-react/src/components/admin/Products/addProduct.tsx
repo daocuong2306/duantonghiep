07ca190456330aaee6d145072d27364adc366727
daocuong2306 <115203956+daocuong2306@users.noreply.github.com>
2023-10-02 22:09:21 +0700

    .

--- Base-Project-react/src/components/admin/Products/addProduct.tsx
+++ Base-Project-react/src/components/admin/Products/addProduct.tsx
@@ -163,13 +163,7 @@ const AddProduct = () => {
                                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray" htmlFor="user_avatar">
                                     Upload file
                                 </label>
-                                <input
-                                    className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-white-50 dark:text-gray-400 focus:outline-none dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400"
-                                    aria-describedby="user_avatar_help"
-                                    id="user_avatar"
-                                    type="file"
-                                    onChange={handleFileChange} // Gắn sự kiện onChange để xử lý khi người dùng chọn tệp
-                                />
+                                 
                             </div>
                         </div>
                         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
