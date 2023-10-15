a1aa8e2b6142ff327d625e2fa703c0f84fc00fc6
DESKTOP-N479MS7\Gia Khanhs <ggia91188@gmail.com>
2023-10-15 07:28:41 +0700

    test option vlaue

--- backend_laravel_farmwork/app/Http/Controllers/Api/OptionValueController.php
+++ backend_laravel_farmwork/app/Http/Controllers/Api/OptionValueController.php
@@ -28,48 +28,6 @@ class OptionValueController extends Controller
                 'isOk' => 'true',
             ], 200);
         }
-<<<<<<< HEAD
-        if (!$keyword) {
-            $option_values = DB::table('option_values')
-                ->join('options', 'option_values.option_id', '=', 'options.id')
-                ->select('option_values.*', 'options.name as options_name')
-                ->get();
-
-            if ($option_values->count() > 0) {
-                return response()->json([
-                    "status" => 200,
-                    "option_values" => $option_values,
-                    'message' => 'dont find',
-                ], 200);
-            } else {
-                return response()->json([
-                    'status' => 200,
-                    'message' => 'not found'
-                ], 400);
-            }
-        }
-
-
-        // $option_values = OptionValue::all();
-
-        //     $option_values = DB::table('option_values')
-        //     ->join('options', 'option_values.option_id', '=', 'options.id')
-        //     ->select('option_values.*', 'options.name as options_name')
-        //     ->get();
-
-        //    if($option_values->count()>0){
-        //     return response()->json([
-        //        "status" => 200,
-        //        "option_values" => $option_values,
-        //     ],200);      
-        //    }else{
-        //     return response()->json([
-        //         'status'=>200,
-        //         'message'=>'not found'
-        //     ],400);
-        //    }
-=======
->>>>>>> b3ea8bee102084fa94b479c0155c4513f500d3af
     }
 
     /**
@@ -82,52 +40,28 @@ class OptionValueController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'option_id' => 'required',
-            'value' => 'required|array', // Thêm kiểm tra value là một mảng
+            'values' => 'required', // Thêm kiểm tra value là một mảng
         ]);
-<<<<<<< HEAD
-
-=======
         // dd($request);
->>>>>>> b3ea8bee102084fa94b479c0155c4513f500d3af
         if ($validator->fails()) {
             return response()->json([
                 'status' => 422,
                 'errors' => $validator->messages(),
             ], 422);
         }
-
-        $option_id = $request->input('option_id');
-        $values = $request->input('value');
-        foreach($values as $value){
-
+        $values = $request->values;
+        foreach ($values as $value) {
+            $option_value = new OptionValue();
+            $option_value->value = $value;
+            $option_value->option_id = $request->option_id;
+            $option_value->save();
         }
-        $encodedValues = json_encode($values);
-        $option_values = OptionValue::create([
-            'option_id' => $option_id,
-            'value' => $encodedValues,
-        ]);
-
-        if ($option_values) {
-            return response()->json([
-                'status' => 200,
-               
-                'message' => 'Successful',
-            ], 200);
-        } else {
-            return response()->json([
-                'status' => 500,
-                'message' => 'Error',
-            ], 500);
-        }
-<<<<<<< HEAD
-=======
         $all = OptionValue::all();
         return response()->json([
             'status' => 200,
             'option_value' => $all,
             'message' => 'Successful',
         ], 200);
->>>>>>> b3ea8bee102084fa94b479c0155c4513f500d3af
     }
 
     /**
@@ -173,34 +107,6 @@ class OptionValueController extends Controller
             ], 404);
         }
     }
-<<<<<<< HEAD
-    public function properties()
-    {
-        // $option_id = $request->input('option_id');
-        // $values = $request->input('value');
-        // return response()->json([
-        //     'option_id' => $option_id,
-        //     'value' => $values,
-        // ]);
-
-    //     $option_values = OptionValue::all();
-    //     $values = $option_values->map(function ($option_value) {
-    //     return json_decode($option_value->value);
-    // });
-
-    // return response()->json([
-    //     'status' => 200,       
-    //     'values' => $values,
-    // ], 200);
-        // $values = json_decode($option_value->value, true);
-        //  return response()->json([
-        //     'option_id' => $option_value,
-        //     'value' => $values,
-        // ]);
-
-    }
-=======
    
     
->>>>>>> b3ea8bee102084fa94b479c0155c4513f500d3af
 }
