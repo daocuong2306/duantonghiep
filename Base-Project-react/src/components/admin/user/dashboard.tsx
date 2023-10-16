9b47241a3d7345e9f41158b15c699fb8d7ece2fc
daocuong2306 <115203956+daocuong2306@users.noreply.github.com>
2023-10-16 23:24:15 +0700

    fix login

--- Base-Project-react/src/components/admin/user/dashboard.tsx
+++ Base-Project-react/src/components/admin/user/dashboard.tsx
@@ -1,5 +1,6 @@
-import { useListUserQuery } from '@/api/user';
-import { Space, Switch, Table, Image } from 'antd';
+import { useBanUserMutation, useListUserQuery, useUnBanUserMutation } from '@/api/user';
+import { Space, Switch, Table, Image, Button } from 'antd';
+import { blue } from '@ant-design/colors';
 import type { ColumnsType } from 'antd/es/table';
 import type { TableRowSelection } from 'antd/es/table/interface';
 import React, { useState } from 'react';
@@ -13,52 +14,9 @@ interface DataType {
     children?: DataType[];
 }
 
-const columns: ColumnsType<DataType> = [
-    {
-        title: 'Ảnh',
-        dataIndex: 'image',
-        key: 'image',
-        width: '10%',
-        render: (image) => <Image height={90} width={100} src='https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp' />,
-    },
-    {
-        title: 'Tên',
-        dataIndex: 'name',
-        key: 'name',
-        width: '20%'
-    },
-    {
-        title: 'Tuổi',
-        dataIndex: 'age',
-        key: 'age',
-        width: '12%',
-    },
-    {
-        title: 'Địa chỉ',
-        dataIndex: 'address',
-        width: '20%',
-        key: 'address',
-    },
-    {
-        title: 'Số điện thoại',
-        dataIndex: 'address',
-        width: '15%',
-        key: 'address',
-    },
-    {
-        title: 'Email',
-        dataIndex: 'email',
-        width: '30%',
-        key: 'email',
-    },
-    {
-        title: 'Quền',
-        dataIndex: 'role',
-        width: '5%',
-        key: 'role',
-    },
-];
 
+
+//unbanuser
 
 
 // rowSelection objects indicates the need for row selection
@@ -75,11 +33,72 @@ const rowSelection: TableRowSelection<DataType> = {
 };
 
 const DashboardUser: React.FC = () => {
+    const columns: ColumnsType<DataType> = [
+        {
+            title: 'Ảnh',
+            dataIndex: 'image',
+            key: 'image',
+            width: '10%',
+            render: (image) => <Image height={90} width={100} src='https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp' />,
+        },
+        {
+            title: 'Tên',
+            dataIndex: 'name',
+            key: 'name',
+            width: '20%'
+        },
+        {
+            title: 'Tuổi',
+            dataIndex: 'age',
+            key: 'age',
+            width: '12%',
+        },
+        {
+            title: 'Địa chỉ',
+            dataIndex: 'address',
+            width: '20%',
+            key: 'address',
+        },
+        {
+            title: 'Số điện thoại',
+            dataIndex: 'address',
+            width: '15%',
+            key: 'address',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            width: '30%',
+            key: 'email',
+        },
+        {
+            title: 'Quền',
+            dataIndex: 'role',
+            width: '5%',
+            key: 'role',
+        },
+        {
+            title: 'Chức năng',
+            dataIndex: 'status',
+            key: 'status',
+            width: '10%',
+            render: (dataIndex, record) => {
+                return (
+                    dataIndex == 0 ? <Button danger onClick={() => banUser(dataIndex)}>
+                        Khóa tài khoản
+                    </Button> : <Button primary onClick={() => unbanUser(dataIndex)}>
+                        Mở tài khoản
+                    </Button>
+                );
+            },
+        },
+    ];
+    const [banUser] = useBanUserMutation();
+    const [unbanUser] = useUnBanUserMutation();
     const { data: user } = useListUserQuery()
-
-    const [checkStrictly, setCheckStrictly] = useState(false);
     console.log(user);
 
+    const [checkStrictly, setCheckStrictly] = useState(false);
     const data: DataType[] = user?.user;
     return (
         <>
