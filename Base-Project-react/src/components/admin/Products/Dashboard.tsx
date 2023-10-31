9fb1261847c73d083b5b04c8d101188c2844835e
daocuong2306 <115203956+daocuong2306@users.noreply.github.com>
2023-10-31 14:08:00 +0700

    .

--- Base-Project-react/src/components/admin/Products/Dashboard.tsx
+++ Base-Project-react/src/components/admin/Products/Dashboard.tsx
@@ -4,12 +4,10 @@ import { useState } from 'react'
 import type { TableColumnsType } from 'antd';
 import { Button, Table, Space, Image } from 'antd';
 import Update from './Update';
-import { useGetValueIdQuery } from '@/api/variant';
 const Dashboard = () => {
     const [find, setFind] = useState({})
     const { data: products } = useGetProductsQuery(find);
     const [deleteProduct] = useRemoveProductMutation()
-    // const { data: variants, isLoading } = useGetValueIdQuery(id)
     const deleteP = (id: number) => {
         const check = window.confirm("Are you sure you want to delete");
         if (check) {
@@ -17,34 +15,7 @@ const Dashboard = () => {
             alert("da xoa")
         }
     }
-    // const data1 = [];
-    // const expandedRowRender = (e) => {
-    //     setId(e.id);
-    //     console.log(variants?.handleVariant);
-    //     const dem = [{ id: e.id, value: Object.values(variants.handleVariant) }];
-    //     data1.push(dem);
-    //     console.log(dem);
-
-    //     const columns: TableColumnsType<any> = [
-    //         {
-    //             title: 'Tên',
-    //             dataIndex: 'option_value',
-    //             key: 'option_value',
-    //             render: (dataIndex) => {
-    //                 if (Array.isArray(dataIndex)) {
-    //                     return dataIndex.join('- ');
-    //                 } else {
-    //                     return dataIndex;
-    //                 }
-    //             }
-    //         },
-    //         { title: 'Giá', dataIndex: 'skus_price', key: 'price' },
-    //         { title: 'Số Lượng', dataIndex: 'stock', key: 'stock' }
-    //     ];
-
-    //     // Sử dụng biến data1 (không định nghĩa lại) thay vì data1 đã định nghĩa ở đầu
-    //     // return <Table columns={columns} dataSource={data1} pagination={false} />;
-    // };
+    
 
     const columns: TableColumnsType<any> = [
         {
