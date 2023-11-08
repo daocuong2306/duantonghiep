f98fced1b9289786817e047ae485fcb1b1874039
daocuong2306 <115203956+daocuong2306@users.noreply.github.com>
2023-11-08 21:24:01 +0700

    home

--- Base-Project-react/src/components/layouts/LayoutWebsite.tsx
+++ Base-Project-react/src/components/layouts/LayoutWebsite.tsx
@@ -1,12 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "../component/layout/header";
 import Footer from "../component/layout/footer";
+import Home from "../component/layout/home";
 
 const LayoutWebsite = () => {
     return <div>
-        <Header/>
-        <Outlet />
-        <Footer />
+        <Home />
     </div>;
 };
 
