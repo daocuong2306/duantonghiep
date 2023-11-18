541e15afd811e40e63c951bf0d584cc83ccb85b3
DESKTOP-N479MS7\Gia Khanhs <ggia91188@gmail.com>
2023-11-18 09:27:47 +0700

    cart

--- backend_laravel_farmwork/app/Http/Controllers/Client/CartdbController.php
+++ backend_laravel_farmwork/app/Http/Controllers/Client/CartdbController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Client;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cart;
+use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
@@ -16,30 +17,35 @@ class CartdbController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        if (Auth::check()) {
-            $user_id = Auth::user()->id;
-            $carts = Cart::where('user_id', $user_id)->get();
-    
-            $formattedCarts = $carts->map(function ($cart) {
-                return [
-                    'id' => $cart->id,
-                    'product_id' => $cart->product_id,
-                ];
-            });
-    
-            $userCart = [
-                'user_id:' . $user_id => $formattedCarts->toArray(),
+{
+    // Kiểm tra xem người dùng đã đăng nhập hay chưa
+    if (Auth::check()) {
+        // Lấy danh sách sản phẩm
+        $products = Product::all();
+
+        // Tạo một mảng chứa thông tin sản phẩm
+        $productList = [];
+        foreach ($products as $product) {
+            $productItem = [
+                'product_id' => $product->id,
+                'product_name' => $product->name,            
+               
+                // Thêm thông tin khác của sản phẩm nếu cần thiết
             ];
-    
-            return response()->json([
-                'carts' => $userCart,
-            ], 200);
-        } else {
-            // Xử lý khi người dùng chưa đăng nhập
+            $productList[] = $productItem;
         }
+
+        return response()->json([
+            'message' => 'Product list ',
+            'products' => $productList,
+        ], 200);
+    } else {
+        return response()->json([
+            'message' => 'Unauthorized',
+        ], 401);
     }
-    
+}
+
     /**
      * Lưu giỏ hàng mới vào cơ sở dữ liệu.
      *
@@ -48,37 +54,36 @@ class CartdbController extends Controller
      */
 
 
-    public function store(Request $request)
-    {
-        // Lấy thông tin người dùng đã đăng nhập
-        $user = Auth::user();
-    
-        // Kiểm tra dữ liệu đầu vào
-        $validator = Validator::make($request->all(), [
-            'product_id' => 'required|exists:product,id',
-            // Các validation rules khác cho các trường khác của cart
-        ]);
-    
-        // Kiểm tra nếu validation không thành công
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 400);
-        }
-    
-        // Tạo một đối tượng cart mới dựa trên dữ liệu từ yêu cầu và user_id
-        $cart = new Cart();
-        $cart->user_id = $user->id;
-        $cart->product_id = $request->input('product_id');
-        // Các trường khác của cart
-    
-        // Lưu thông tin giỏ hàng
-        $cart->save();
-    
-        // Trả về cart vừa được tạo làm dữ liệu JSON
-        return response()->json($cart, 201);
-    }
+     public function store(Request $request)
+     {
+         // Lấy danh sách các product id từ bảng 'product'
+         $productIds = Product::pluck('id')->toArray();
+     
+         // Kiểm tra và validate 'product_ids'
+         $validatedData = $request->validate([
+             'product_ids' => 'required|array|in:' . implode(',', $productIds),
+         ]);
+     
+         // Lấy thông tin sản phẩm từ các product_ids
+         $products = Product::whereIn('id', $validatedData['product_ids'])->get();
+     
+         // Tạo một mảng carts chứa thông tin sản phẩm
+         $carts = [];
+         foreach ($products as $product) {
+             $cartItem = [
+                 'product_id' => $product->id,
+                 'product_name' => $product->name,
+                 // Thêm thông tin khác của sản phẩm nếu cần thiết
+             ];
+             $carts[] = $cartItem;
+         }
+     
+         return response()->json([
+             'carts' => $carts,
+         ], 201);
+     }
 
-    
-    
+
     /**
      * Cập nhật giỏ hàng.
      *
@@ -86,40 +91,40 @@ class CartdbController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-   
-public function update(Request $request, $id)
-{
-    // Lấy thông tin người dùng đã đăng nhập
-    $user = Auth::user();
 
-    // Kiểm tra dữ liệu đầu vào
-    $validator = Validator::make($request->all(), [
-        'product_id' => 'required|exists:product,id',
-        // Các validation rules khác cho các trường khác của cart
-    ]);
+    public function update(Request $request, $id)
+    {
+        // Lấy thông tin người dùng đã đăng nhập
+        $user = Auth::user();
 
-    // Kiểm tra nếu validation không thành công
-    if ($validator->fails()) {
-        return response()->json(['errors' => $validator->errors()], 400);
+        // Kiểm tra dữ liệu đầu vào
+        $validator = Validator::make($request->all(), [
+            'product_id' => 'required|exists:product,id',
+            // Các validation rules khác cho các trường khác của cart
+        ]);
+
+        // Kiểm tra nếu validation không thành công
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        // Kiểm tra nếu cart tồn tại và thuộc về người dùng hiện tại
+        $cart = Cart::where('id', $id)->where('user_id', $user->id)->first();
+        if (!$cart) {
+            return response()->json(['error' => 'Cart not found'], 404);
+        }
+
+        // Cập nhật thông tin giỏ hàng
+        $cart->product_id = $request->input('product_id');
+        // Cập nhật các trường khác của cart
+
+        // Lưu thông tin giỏ hàng đã được cập nhật
+        $cart->save();
+
+        // Trả về cart đã được cập nhật làm dữ liệu JSON
+        return response()->json($cart, 200);
     }
 
-    // Kiểm tra nếu cart tồn tại và thuộc về người dùng hiện tại
-    $cart = Cart::where('id', $id)->where('user_id', $user->id)->first();
-    if (!$cart) {
-        return response()->json(['error' => 'Cart not found'], 404);
-    }
-
-    // Cập nhật thông tin giỏ hàng
-    $cart->product_id = $request->input('product_id');
-    // Cập nhật các trường khác của cart
-
-    // Lưu thông tin giỏ hàng đã được cập nhật
-    $cart->save();
-
-    // Trả về cart đã được cập nhật làm dữ liệu JSON
-    return response()->json($cart, 200);
-}
-    
     /**
      * Xóa giỏ hàng khỏi cơ sở dữ liệu.
      *
@@ -129,10 +134,10 @@ public function update(Request $request, $id)
     public function destroy($id)
     {
         $cart = Cart::find($id);
-    
+
         if ($cart) {
             $cart->delete();
-    
+
             return response()->json([
                 'status' => 200,
                 'message' => 'Xóa giỏ hàng thành công',
--- backend_laravel_farmwork/database/migrations/2023_10_26_065105_create_carts_table.php
+++ backend_laravel_farmwork/database/migrations/2023_10_26_065105_create_carts_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
-            $table->integer('product_id');
+            $table->json('product_ids')->nullable();
+            // $table->integer('product_id');
             // $table->integer('sku_id');
             // $table->integer('quantity');
             // $table->integer('price_cart')->nullable();
