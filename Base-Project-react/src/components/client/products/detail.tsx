b217aad58ed96f6fcb52df5c92e2c9ec2e2c5739
datnt210803 <137571347+datnt210803@users.noreply.github.com>
2023-11-21 18:00:28 +0700

    latest comment
    latest comment

--- Base-Project-react/src/components/client/products/detail.tsx
+++ Base-Project-react/src/components/client/products/detail.tsx
@@ -235,8 +235,9 @@ export default function DetailProduct() {
                     <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
                     </div>
                 </div >
-                <Showcomt data={detaiProduct?.data.comment} />
+                
                 <Comment />
+                <Showcomt data={detaiProduct?.data.comment} />
             </div >
         </Spin>
     )
