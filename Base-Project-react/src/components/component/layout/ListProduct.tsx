7842d2712de84255c079a2c300c38153969ec78d
datnt210803 <137571347+datnt210803@users.noreply.github.com>
2023-11-28 14:44:16 +0700

    home
    home

--- Base-Project-react/src/components/component/layout/ListProduct.tsx
+++ Base-Project-react/src/components/component/layout/ListProduct.tsx
@@ -10,25 +10,25 @@ export default function Example() {
         <div className="container-fluid pt-5">
             <div className="flex flex-wrap px-5 pb-3">
                 <div className="w-full md:w-1/2 lg:w-1/4 pb-1">
-                    <div className="flex items-center border mb-4 p-6">
+                    <div className="flex items-center border mb-4 p-6 h-[100%]">
                         <i className="fa fa-check text-[#00ccff] text-3xl mr-3"></i>
                         <h5 className="font-semibold m-0 text-[#00ccff]">Sản phẩm chất lượng</h5>
                     </div>
                 </div>
                 <div className="w-full md:w-1/2 lg:w-1/4 pb-1">
-                    <div className="flex items-center border mb-4 p-6">
+                    <div className="flex items-center border mb-4 p-6 h-[100%]">
                         <i className="fa fa-shipping-fast text-[#00ccff] text-2xl mr-2"></i>
                         <h5 className="font-semibold m-0 text-[#00ccff]">Miễn phí vận chuyển</h5>
                     </div>
                 </div>
                 <div className="w-full md:w-1/2 lg:w-1/4 pb-1">
-                    <div className="flex items-center border mb-4 p-6">
+                    <div className="flex items-center border mb-4 p-6 h-[100%]">
                         <i className="fas fa-exchange-alt text-[#00ccff] text-3xl mr-3"></i>
                         <h5 className="font-semibold m-0 text-[#00ccff]">7 ngày đổi trả</h5>
                     </div>
                 </div>
                 <div className="w-full md:w-1/2 lg:w-1/4 pb-1">
-                    <div className="flex items-center border mb-4 p-6">
+                    <div className="flex items-center border mb-4 p-6 h-[100%]">
                         <i className="fa fa-phone-volume text-[#00ccff] text-3xl mr-3"></i>
                         <h5 className="font-semibold m-0 text-[#00ccff]">Tư vấn tận tâm</h5>
                     </div>
