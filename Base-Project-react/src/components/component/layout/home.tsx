ca8da8b708ca8f6f79a02ea9fb86bb27bfe6ff78
datnt210803 <137571347+datnt210803@users.noreply.github.com>
2023-11-30 18:04:30 +0700

    fix html
    fix html

--- Base-Project-react/src/components/component/layout/home.tsx
+++ Base-Project-react/src/components/component/layout/home.tsx
@@ -23,7 +23,7 @@ const Home = (props: Props) => {
             <div className=''>
                 <body>
                     <Banner />
-                    <Example />
+                    {/* <Example /> */}
                     <div className="container-fluid pt-5">
                         <div className="row px-xl-5 pb-3">
                             <ListCate />
@@ -70,39 +70,7 @@ const Home = (props: Props) => {
                         </div>
                         <NewProducts />
                     </div>
-                    <div className="container-fluid py-5">
-                        <div className="row px-xl-5">
-                            <div className="col">
-                                <div className="owl-carousel vendor-carousel flex">
-                                    <div className="vendor-item border p-4">
-                                        <img src="https://themewagon.github.io/eshopper/img/vendor-1.jpg" alt="" />
-                                    </div>
-                                    <div className="vendor-item border p-4">
-                                        <img src="https://themewagon.github.io/eshopper/img/vendor-2.jpg" alt="" />
-                                    </div>
-                                    <div className="vendor-item border p-4">
-                                        <img src="https://themewagon.github.io/eshopper/img/vendor-3.jpg" alt="" />
-                                    </div>
-                                    <div className="vendor-item border p-4">
-                                        <img src="https://themewagon.github.io/eshopper/img/vendor-4.jpg" alt="" />
-                                    </div>
-                                    <div className="vendor-item border p-4">
-                                        <img src="https://themewagon.github.io/eshopper/img/vendor-5.jpg" alt="" />
-                                    </div>
-                                    <div className="vendor-item border p-4">
-                                        <img src="https://themewagon.github.io/eshopper/img/vendor-6.jpg" alt="" />
-                                    </div>
-                                    <div className="vendor-item border p-4">
-                                        <img src="https://themewagon.github.io/eshopper/img/vendor-7.jpg" alt="" />
-                                    </div>
-                                    <div className="vendor-item border p-4">
-                                        <img src="https://themewagon.github.io/eshopper/img/vendor-8.jpg" alt="" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
+                   
 
                     <Link to="#" className="btn btn-primary back-to-top"><i className="fa fa-angle-double-up"></i></Link>
                 </body>
